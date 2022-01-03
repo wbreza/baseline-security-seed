5a43448568b8c6c272fdf63afcb2bdcb5146c7f9
Wallace Breza <wallace.breza@microsoft.com>
2022-01-03 15:03:49 -0800

    feat: Leverage generated scripts from detect-secrets pre-commit hook repo (#5)
    In an effort to be more DRY, leverages the scripts generated from the https://github.com/wbreza/pre-commit-hooks/tree/main/detect-secrets script.

--- .devcontainer/scripts/container-postCreate.sh
+++ .devcontainer/scripts/container-postCreate.sh
@@ -13,13 +13,10 @@ cd "$parent_path"
 echo "Changing owner on .git/hooks directory..."
 sudo chown codespace -R ../../.git/hooks
 
-sudo apt install python3.8-venv
+sudo apt install -y python3.8-venv
 
-# Create & activate python virtual environment
-. ./python-virtualenv.sh
-
-# Initialize Git pre-commit hooks
-. ./init-repo.sh
+# Activate & install detect-secrets
+. ../../scripts/detect-secrets/init.sh
 
 # Detect and run oryx build from universal dev container
 oryx build -p virtualenv_name=.venv --log-file /tmp/oryx-build.log --manifest-dir /tmp || echo 'Could not auto-build. Skipping.'
